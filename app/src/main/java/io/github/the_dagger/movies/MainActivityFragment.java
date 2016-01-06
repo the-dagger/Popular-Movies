86ac29263de01aa6fe59381aa12be5457070367f
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-01-07 00:51:32 +0530

    Implement AsyncTask to fetch Posters and Movie Names

--- app/src/main/java/io/github/the_dagger/movies/MainActivityFragment.java
+++ app/src/main/java/io/github/the_dagger/movies/MainActivityFragment.java
@@ -1,12 +1,26 @@
 package io.github.the_dagger.movies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 
 /**
@@ -18,7 +32,11 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
 
     }
-
+    HttpURLConnection urlConnection = null;
+    BufferedReader reader = null;
+    // Will contain the raw JSON response as a string.
+    String movieinfo = null;
+    private final String LOG_TAG = MovieDetails.class.getSimpleName();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,6 +50,104 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         GridView gridview = (GridView) rootView.findViewById(R.id.gridView);
         gridview.setAdapter(adapter);
+
         return rootView;
     }
-}
+    public class MovieDetails extends AsyncTask<Void, Void, SingleMovie[]> {
+        private SingleMovie[] getmovieData(String movieDbUrl)
+                throws JSONException {
+            JSONObject moviejson = new JSONObject(movieDbUrl);
+            JSONArray movieArray = moviejson.getJSONArray("results");
+            String baseURL = "http://image.tmdb.org/t/p/w185/";
+            SingleMovie[] movieDetails = new SingleMovie[20];
+            for(int i =0;i<movieArray.length();i++){
+                String title;
+                int poster;
+            JSONObject currentMovie = movieArray.getJSONObject(i);
+                JSONObject movietitle = currentMovie.getJSONObject("title");
+                JSONObject moviePosterendURL = currentMovie.getJSONObject("poster_path");
+                String moviePosterURL = baseURL + moviePosterendURL;
+                String actualTitle = movietitle.getString("title");
+                int posterpicasso = Picasso.with(getContext()).load(moviePosterURL);
+                return movieDetails[i] = (,actualTitle);
+            }
+
+        }
+
+        @Override
+        protected SingleMovie[] doInBackground(Void... params) {
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9ee088a6d3ed11d3c10ee27466d39427");
+//                Uri.Builder builder = new Uri.Builder();
+//                builder.scheme("http")
+//                        .authority("api.openweathermap.org")
+//                        .appendPath("data")
+//                        .appendPath("2.5")
+//                        .appendPath("forecast")
+//                        .appendPath("daily")
+//                        .appendQueryParameter("q", String.valueOf(params[0]))
+//                        .appendQueryParameter("mode", "json")
+//                        .appendQueryParameter("units", "metric")
+//                        .appendQueryParameter("cnt", "7")
+//                        .appendQueryParameter("appid", "232730d9c646236b0cf445becaaf2240");
+                String actualURL = url.toString();
+                Log.v(LOG_TAG, actualURL);
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieinfo = buffer.toString();
+                Log.v(LOG_TAG, movieinfo);
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getmovieData(movieinfo);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
+    }
