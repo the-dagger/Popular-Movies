8bb348d7291f88ee28ff6fcd61965b46c4bd53ba
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-01-07 15:00:21 +0530

    MaterialYOLO

--- app/src/main/java/io/github/the_dagger/movies/MovieAdapter.java
+++ app/src/main/java/io/github/the_dagger/movies/MovieAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.florent37.picassopalette.PicassoPalette;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -21,11 +22,12 @@ public class MovieAdapter extends ArrayAdapter<SingleMovie>{
         SingleMovie singleMovie = getItem(position);
         View rootView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
         ImageView poster = (ImageView) rootView.findViewById(R.id.movie_poster_image);
-        Picasso.with(getContext()).load(singleMovie.movieImage).into(poster);
         TextView name = (TextView) rootView.findViewById(R.id.movie_name);
+        Picasso.with(getContext()).load(singleMovie.movieImage).into(poster, PicassoPalette.with(singleMovie.movieImage,poster).use(PicassoPalette.Profile.MUTED_LIGHT).intoBackground(name));
         name.setText(singleMovie.movieTitle);
         return rootView;
     }
+
 
     public MovieAdapter(FragmentActivity context, List<SingleMovie> resource) {
         super(context, 0, resource);
