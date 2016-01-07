cc3abcdf23d9e78260c31bd0abaf6e23789a6eb1
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-01-07 14:36:50 +0530

    Improve the layout.

--- app/src/main/java/io/github/the_dagger/movies/MainActivityFragment.java
+++ app/src/main/java/io/github/the_dagger/movies/MainActivityFragment.java
@@ -48,9 +48,6 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -77,7 +74,8 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         GridView gridview = (GridView) rootView.findViewById(R.id.gridView);
         gridview.setAdapter(adapter);
-
+        MovieDetails weather = new MovieDetails();
+        weather.execute();
         return rootView;
     }
 
@@ -101,17 +99,13 @@ public class MainActivityFragment extends Fragment {
             final String MDB_POSTER = "poster_path";
             JSONObject moviejson = new JSONObject(movieInfo);
             JSONArray movieArray = moviejson.getJSONArray(MDB_RESULT);
-          //  Log.e(LOG_TAG, String.valueOf(movieArray));
             String baseURL = "http://image.tmdb.org/t/p/w185/";
             SingleMovie[] movieDetails = new SingleMovie[20];
             for (int i = 0; i < 20; i++) {
                 JSONObject currentMovie = movieArray.getJSONObject(i);
-              //  Log.e(LOG_TAG, String.valueOf(currentMovie));
                 String movietitle = currentMovie.getString(MDB_TITLE);
                 String moviePosterendURL = currentMovie.getString(MDB_POSTER);
                 String moviePosterURL = baseURL + moviePosterendURL;
-             //   Log.e(LOG_TAG,moviePosterURL);
-                //String actualTitle = movietitle.getString(MDB_TITLE);
                 movieDetails[i] = new SingleMovie(moviePosterURL, movietitle);
             }
             return movieDetails;
@@ -120,30 +114,13 @@ public class MainActivityFragment extends Fragment {
         @Override
         protected SingleMovie[] doInBackground(Void... params) {
             try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
                 URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9ee088a6d3ed11d3c10ee27466d39427");
-//                Uri.Builder builder = new Uri.Builder();
-//                builder.scheme("http")
-//                        .authority("api.openweathermap.org")
-//                        .appendPath("data")
-//                        .appendPath("2.5")
-//                        .appendPath("forecast")
-//                        .appendPath("daily")
-//                        .appendQueryParameter("q", String.valueOf(params[0]))
-//                        .appendQueryParameter("mode", "json")
-//                        .appendQueryParameter("units", "metric")
-//                        .appendQueryParameter("cnt", "7")
-//                        .appendQueryParameter("appid", "232730d9c646236b0cf445becaaf2240");
                 String movieDbUrl = url.toString();
                 Log.v(LOG_TAG, movieDbUrl);
-                // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
-                // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
                 StringBuilder buffer = new StringBuilder();
                 if (inputStream == null) {
